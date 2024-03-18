1ed395a9757e7428bbc7ff9b979694456f25b32b
Aelkoulali <abdellah.elkoulali@gmx.fr>
2024-03-18 21:47:40 +0100

    Add Functions

--- script.js
+++ script.js
@@ -5,3 +5,15 @@ const entryDropdown = document.getElementById('entry-dropdown');
 const addEntryButton = document.getElementById('add-entry');
 const clearButton = document.getElementById('clear');
 const output = document.getElementById('output');
+// Declare Variable IsError and Set it to False
+let isError = false; 
+// functions
+function cleanInputString(){
+    const regex = /[+-\s]/g;
+    return str.replace(regex, '');
+}
+
+function InInvalidInput(){
+    const regex = /\d+e\d+/i;
+    return str.match(regex);
+}
